e7aedf7693d9199f1b18a739a7ae3bdcf6223e82
U-UZ\litl <litl@UPC1561.uz.local>
2016-04-28 15:10:06 +0800

    init java web project

---
+++ src/com/lity/org/servlet/Index.java
@@ -0,0 +1,34 @@
+package com.lity.org.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Index
+ */
+@WebServlet("/Index")
+public class Index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Index() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		request.get
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getOutputStream().write("<p>hello, world</p>".getBytes());
+//		response.
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
